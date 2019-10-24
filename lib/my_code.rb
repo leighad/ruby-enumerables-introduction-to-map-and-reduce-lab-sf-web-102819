6663febd77f0768794c711a18a8792aa8f65afc3
Leigha De La Rosa <cdversiontwo@gmail.com>
2019-10-24 05:04:57 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -55,13 +55,14 @@ end
 
 def reduce_to_all_true(array)
   # array.reduce { |el| el == true}
-  i = 0 
+  i = 0
   while i < array.length do
     if array[i] === true
       true
     end
   i += 1 
   end
+  true
 end
 
 def reduce_to_any_true(array)
