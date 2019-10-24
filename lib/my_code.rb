9ddf0c7cc4f62b7840c9dcb1ec62f6764a8ea81a
Leigha De La Rosa <cdversiontwo@gmail.com>
2019-10-24 04:53:55 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,75 @@
-# My Code here....
+# My Code here....DON'T USE MAP OR REDUCE!!!
+def map_to_negativize(array)
+  # array.map{ |n| n * -1}
+  i = 0
+  new_array = []
+  while i < array.length do
+    new_array << array[i] * -1
+    i += 1
+  end
+  new_array
+end
+
+def map_to_no_change(array)
+  # array.map{ |el| el}
+  i = 0 
+  while i < array.length do
+    array[i]
+    i += 1
+  end
+  array
+end
+
+def map_to_double(array)
+  # array.map{ |n| n * 2}
+  i = 0 
+  new_array = []
+  while i < array.length do
+    new_array << array[i] * 2
+    i += 1
+  end
+  new_array
+end
+
+def map_to_square(array)
+  # array.map{ |n| n * n}
+  i = 0 
+  new_array = []
+  while i < array.length do
+    new_array << array[i] * array[i]
+    i += 1
+  end
+  new_array
+end
+
+def reduce_to_total(array, start = 0)
+  # array.reduce(start) { |sum, n| sum + n}
+  i = 0 
+  total = start 
+  while i < array.length do
+    total += array[i]
+    i += 1 
+  end
+  total
+end
+
+def reduce_to_all_true(array)
+  # array.reduce { |el| el == true}
+  i = 0 
+  while i < array.length do
+    if array[i] === true
+      true
+    end
+  i += 1 
+  end
+end
+
+def reduce_to_any_true(array)
+  # array.reduce { |el| el === true}
+end
+
+
+
+
+
+
